0c7a52cc5507116180577328ef55662c23869a41
Corfaya <lauracorrado0696@gmail.com>
2024-09-27 20:27:25 +0200

    [UPD] MILESTONE 1: columns definition and migration execution

--- database/migrations/2024_09_27_180147_create_comics_table.php
+++ database/migrations/2024_09_27_180147_create_comics_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->decimal('price', 5, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->json('artists');
+            $table->json('writers');
             $table->timestamps();
         });
     }
