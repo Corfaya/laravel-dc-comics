dbf58c66ca2e1cc3a258fde841cd47bff333985a
Corfaya <lauracorrado0696@gmail.com>
2024-09-27 20:54:13 +0200

    [UPD] faker seeds created

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic as Comic;
+use Faker\Generator as Faker;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -12,8 +14,31 @@ class ComicsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        //test
+        $artists = [];
+        $writers = [];
+        for($a=0; $a<5; $a++) {
+            $artists [] = $faker->firstName();
+        }
+        for($w=0; $w<5; $w++) {
+            $writers [] = $faker->firstName();
+        }
+        //end test
+
+        for($k=0; $k<30; $k++) {
+            $comic = new Comic();
+
+            $comic->title = $faker->words(3, true);
+            $comic->description = $faker->realText($maxNbChars = 300, $indexSize = 3);
+            $comic->thumb = $faker->word.'/'.$faker->word().'.jpg';
+            $comic->price = $faker->randomFloat(2, 1, 350).'€';
+            $comic->series = $faker->words(4, true);
+            $comic->sale_date = $faker->date();
+            $comic->type = $faker->words(2, true);
+            $comic->artists = json_encode($faker->randomElements($artists));
+            $comic->writers = $faker->json_encode($faker->randomElements($writers));
+        }
     }
 }
