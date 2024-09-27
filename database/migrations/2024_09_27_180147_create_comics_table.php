261fa7e6b95080a544bb6307cb66599619d69986
Corfaya <lauracorrado0696@gmail.com>
2024-09-27 21:01:58 +0200

    fix: fix json_encode and json type in seeder and migration

--- database/migrations/2024_09_27_180147_create_comics_table.php
+++ database/migrations/2024_09_27_180147_create_comics_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->string('series');
             $table->date('sale_date');
             $table->string('type', 50);
-            $table->json('artists');
-            $table->json('writers');
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -19,11 +19,11 @@ class ComicsTableSeeder extends Seeder
         //test
         $artists = [];
         $writers = [];
-        for($a=0; $a<5; $a++) {
-            $artists [] = $faker->firstName();
+        for($a=0; $a<20; $a++) {
+            $artists [] = $faker->firstName().' '.$faker->lastName();
         }
-        for($w=0; $w<5; $w++) {
-            $writers [] = $faker->firstName();
+        for($w=0; $w<20; $w++) {
+            $writers [] = $faker->firstName().' '.$faker->lastName();
         }
         //end test
 
@@ -33,12 +33,14 @@ class ComicsTableSeeder extends Seeder
             $comic->title = $faker->words(3, true);
             $comic->description = $faker->realText($maxNbChars = 300, $indexSize = 3);
             $comic->thumb = $faker->word.'/'.$faker->word().'.jpg';
-            $comic->price = $faker->randomFloat(2, 1, 350).'€';
+            $comic->price = $faker->randomFloat(2, 1, 350);
             $comic->series = $faker->words(4, true);
             $comic->sale_date = $faker->date();
             $comic->type = $faker->words(2, true);
-            $comic->artists = json_encode($faker->randomElements($artists));
-            $comic->writers = $faker->json_encode($faker->randomElements($writers));
+            $comic->artists = implode(', ', $faker->randomElements($artists));
+            $comic->writers = implode(', ', $faker->randomElements($writers));
+
+            $comic->save();
         }
     }
 }
