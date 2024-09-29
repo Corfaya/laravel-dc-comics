11fc006ac4cd6214e7a0f696855d21a15a6d4df4
Corfaya <lauracorrado0696@gmail.com>
2024-09-29 23:22:52 +0200

    [UPD] detail page completed

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -9,7 +9,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="d-flex mt-5">
+                <div class="d-flex my-5">
                     <div class="details my-5 me-5 py-2">
                         <h2>{{$comic->title}}</h2>
                         <div class="green-bar px-4 my-3">
@@ -45,6 +45,42 @@
                     <div class="advertisment my-5 d-none d-md-block">
                         <p class="text-end text-uppercase text-secondary m-0">Advertisement</p>
                         <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="Pubblicità">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-details py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <h5 class="fw-bold">Talent</h5>
+                    <div class="p-1">
+                        <div class="d-flex justify-content-between fw-bolder">
+                            <p class="m-0 pe-5">Art by:</p>
+                            <p class="text-primary ps-5">{{$comic->artists}}</p>
+                        </div>
+                        <div class="d-flex justify-content-between fw-bolder">
+                            <p class="m-0 pe-5">Written by:</p>
+                            <p class="text-primary ps-5">{{$comic->writers}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-5 offset-md-1">
+                    <h5 class="fw-bold">Specs</h5>
+                    <div class="p-1">
+                        <div class="d-flex justify-content-between fw-bolder">
+                            <p class="m-0 pe-5">Series:</p>
+                            <p class="text-primary text-uppercase ps-5">{{$comic->series}}</p>
+                        </div>
+                        <div class="d-flex justify-content-between fw-bolder">
+                            <p class="m-0 pe-5">U.S. Price:</p>
+                            <p class="text-primary ps-5">${{$comic->price}}</p>
+                        </div>
+                        <div class="d-flex justify-content-between fw-bolder">
+                            <p class="m-0 pe-5">On Sale Date:</p>
+                            <p class="text-primary ps-5">{{$comic->sale_date}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
