6fd39770bff8afbfc644bc037a1aa927a189bff9
Corfaya <lauracorrado0696@gmail.com>
2024-09-29 22:48:13 +0200

    [UPD] comic detail top done

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,46 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h1>I nostri fumetti</h1>
-                    <a href="{{route('comics.create')}}" class="btn btn-warning btn-sm">Add</a>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-12">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope='col'>#</th>
-                            <th scope='col'>Titolo</th>
-                            <th scope='col'>Prezzo</th>
-                            <th scope='col'>Serie</th>
-                            <th scope='col'>Data di vendita</th>
-                            <th scope='col'>Tools</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($comics as $comic)
-                        <tr>
-                            <td>{{$comic->id}}</td>
-                            <td>{{$comic->title}}</td>
-                            <td>{{$comic->price}}€</td>
-                            <td>{{$comic->series}}</td>
-                            <td>{{$comic->sale_date}}</td>
-                            <td>
-                                <a href="{{route('comics.show', ['comic'=> $comic->id])}}" class="btn btn-sm btn-primary">MOSTRA</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div> --}}
     <div class="bg-hero">
         <button class="btn main-btn text-uppercase fw-bolder text-white rounded-0 position-relative d-none d-md-block">Current Series</button>
     </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,10 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="bg-hero">
+    <div class="detail-cover-box position-relative d-none d-md-block"><img src="{{$comic->thumb}}"></img></div>
+</div>
+<div class="banner py-4">
+</div>
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>{{$comic->title}}</h1>
+                <div class="d-flex mt-5">
+                    <div class="details my-5 me-5 py-2">
+                        <h2>{{$comic->title}}</h2>
+                        <div class="green-bar px-4 my-3">
+
+                            <div class="d-flex justify-content-between">
+
+                                <p class="m-0 price-tag py-2">U.S. Price: <span class="price text-white fw-bold">${{$comic->price}}</span></p>
+
+                                <div class="d-flex">
+                                    <p class="text-uppercase m-0 available-style fw-bolder pe-3 py-2">Available</p>
+
+                                    <div class="dropdown ps-3 py-2">
+                                        <a class="dropdown-toggle text-white fw-bold text-decoration-none" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Check Availability
+                                        </a>
+                                          <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                          </ul>
+                                          <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                          </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                            
+                        </div>
+                        
+                        
+                        <p>{{$comic->description}} </p>
+                    </div>
+
+                    <div class="advertisment my-5 d-none d-md-block">
+                        <p class="text-end text-uppercase text-secondary m-0">Advertisement</p>
+                        <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="Pubblicità">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
