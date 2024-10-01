f035b92978e3ca88361a1534af06f8fb87faaa62
Corfaya <lauracorrado0696@gmail.com>
2024-10-01 15:46:43 +0200

    [UPD] test requests

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comic as Comic;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
 
 class ComicController extends Controller
 {
@@ -34,42 +36,11 @@ class ComicController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {   
-        //validation
-        $request->validate([
-            'title' => 'required|max:100',
-            'thumb' => 'max:255',
-            'price' => 'decimal:2',
-            'series' => 'required|max:255',
-            'sale_date' => 'date',
-            'artists' => 'required',
-            'writers' => 'required'
-        ],
-        [
-            'title.required' => 'Questo campo è obbligatorio',
-            'title.max' => 'Il numero massimo di caratteri è 100',
-            'thumb.max' => 'Il numero massimo di caratteri è 255',
-            'price.decimal' => 'Puoi inserire massimo due numeri dopo la virgola',
-            'series.required' => 'Questo campo è obbligatorio',
-            'series.max' => 'Il numero massimo di caratteri è 255',
-            'sale_date.date' => 'Deve essere una data in formato gg/mm/aaaa',
-            'artists.required' => 'Questo campo è obbligatorio',
-            'writers.required' => 'Questo campo è obbligatorio',
-        ]);
-
-        $form_data = $request->all();
+        $form_data = $request->validated();
         $comic = new Comic();
 
-        // $comic->title = $form_data['title'];
-        // $comic->description = $form_data['description'];
-        // $comic->thumb = $form_data['thumb'];
-        // $comic->price = $form_data['price'];
-        // $comic->series = $form_data['series'];
-        // $comic->sale_date = $form_data['sale_date'];
-        // $comic->type = $form_data['type'];
-        // $comic->artists = $form_data['artists'];
-        // $comic->writers = $form_data['writers'];
         $comic->fill($form_data);
         $comic->save();
 
@@ -106,36 +77,12 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Comic $comic)
+    public function update(UpdateComicRequest $request, Comic $comic)
     {   
-        //validation
-        $request->validate([
-            'title' => 'required|max:100',
-            'thumb' => 'max:255',
-            'price' => 'decimal:2',
-            'series' => 'required|max:255',
-            'sale_date' => 'date',
-            'type' => 'required|max:50',
-            'artists' => 'required',
-            'title' => 'required'
-        ],
-        [
-            'title.required' => 'Questo campo è obbligatorio',
-            'title.max' => 'Il numero massimo di caratteri è 100',
-            'thumb.max' => 'Il numero massimo di caratteri è 255',
-            'price.decimal' => 'Puoi inserire massimo due numeri dopo la virgola',
-            'series.required' => 'Questo campo è obbligatorio',
-            'series.max' => 'Il numero massimo di caratteri è 255',
-            'sale_date.date' => 'Deve essere una data in formato gg/mm/aaaa',
-            'type.required' => 'Questo campo è obbligatorio',
-            'type.max' => 'Il numero massimo di caratteri è 50',
-            'artists.required' => 'Questo campo è obbligatorio',
-            'writers.required' => 'Questo campo è obbligatorio',
-        ]);
-
-        $form_data = $request->all();
-        $comic->fill($form_data);
-        $comic->update();
+        
+        $form_data = $request->validated();
+        // $comic->fill($form_data);
+        $comic->update($form_data);
         return redirect()->route('comics.show', compact('comic'));
     }
 
---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            [
+                'title' => 'required|max:100',
+                'thumb' => 'max:255',
+                'price' => 'decimal:2',
+                'series' => 'required|max:255',
+                'sale_date' => 'date',
+                'artists' => 'required',
+                'writers' => 'required'
+            ]
+        ];
+    }
+
+    public function messages() {
+        return [
+            [
+                'title.required' => 'Questo campo è obbligatorio',
+                'title.max' => 'Il numero massimo di caratteri è 100',
+                'thumb.max' => 'Il numero massimo di caratteri è 255',
+                'price.decimal' => 'Puoi inserire massimo due numeri dopo la virgola',
+                'series.required' => 'Questo campo è obbligatorio',
+                'series.max' => 'Il numero massimo di caratteri è 255',
+                'sale_date.date' => 'Deve essere una data in formato gg/mm/aaaa',
+                'artists.required' => 'Questo campo è obbligatorio',
+                'writers.required' => 'Questo campo è obbligatorio',
+            ]
+            ];
+    }
+}
